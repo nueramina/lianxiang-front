471e19dbf0e60a42c623c6b5c07e8ee6aa262f6b
zhangyezi <zhangyezi@jiuzhilan.com>
2020-05-26 21:13:48 +0800

    添加列表样式

--- src/components/page/BaseTable.vue
+++ src/components/page/BaseTable.vue
@@ -25,54 +25,47 @@
             </div>
             <el-table
                 :data="tableData"
-                border
-                class="table"
+                stripe
+                class="table list-table"
                 ref="multipleTable"
                 header-cell-class-name="table-header"
                 @selection-change="handleSelectionChange"
             >
-                <el-table-column type="selection" width="55" align="center"></el-table-column>
-                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-                <el-table-column prop="name" label="用户名"></el-table-column>
-                <el-table-column label="账户余额">
+                <el-table-column prop="name" label="数据信息"></el-table-column>
+                <el-table-column label="实施日期">
                     <template slot-scope="scope">￥{{scope.row.money}}</template>
                 </el-table-column>
-                <el-table-column label="头像(查看大图)" align="center">
-                    <template slot-scope="scope">
-                        <el-image
-                            class="table-td-thumb"
-                            :src="scope.row.thumb"
-                            :preview-src-list="[scope.row.thumb]"
-                        ></el-image>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
+                <el-table-column prop="name" label="测试人数"></el-table-column>
+
+                <el-table-column prop="address" label="提示词"></el-table-column>
                 <el-table-column label="状态" align="center">
                     <template slot-scope="scope">
-                        <el-tag
-                            :type="scope.row.state==='成功'?'success':(scope.row.state==='失败'?'danger':'')"
-                        >{{scope.row.state}}</el-tag>
+                        {{scope.row.state}}
                     </template>
                 </el-table-column>
 
-                <el-table-column prop="date" label="注册时间"></el-table-column>
-                <el-table-column label="操作" width="180" align="center">
+                <el-table-column label="操作" width="250" align="center">
                     <template slot-scope="scope">
-                        <el-button
-                            type="text"
-                            icon="el-icon-edit"
-                            @click="handleEdit(scope.$index, scope.row)"
-                        >编辑</el-button>
-                        <el-button
-                            type="text"
-                            icon="el-icon-delete"
-                            class="red"
-                            @click="handleDelete(scope.$index, scope.row)"
-                        >删除</el-button>
+                        <div class="option-btn">整列</div>
+                        <div class="option-btn">增加</div>
+                        <div class="option-btn">修改</div>
+                        <div class="option-btn">删除</div>
+
+                        <!-- <el-button
+                             type="text"
+                             icon="el-icon-edit"
+                             @click="handleEdit(scope.$index, scope.row)"
+                         >编辑</el-button>
+                         <el-button
+                             type="text"
+                             icon="el-icon-delete"
+                             class="red"
+                             @click="handleDelete(scope.$index, scope.row)"
+                         >删除</el-button>-->
                     </template>
                 </el-table-column>
             </el-table>
-            <div class="pagination">
+            <!--<div class="pagination">
                 <el-pagination
                     background
                     layout="total, prev, pager, next"
@@ -81,7 +74,7 @@
                     :total="pageTotal"
                     @current-change="handlePageChange"
                 ></el-pagination>
-            </div>
+            </div>-->
         </div>
 
         <!-- 编辑弹出框 -->
